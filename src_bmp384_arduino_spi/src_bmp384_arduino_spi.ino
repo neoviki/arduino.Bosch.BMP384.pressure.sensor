b97fada39e07af4cd34909390e10b35f98ba3298
Vignesh Natarajan <vikiworks.io@gmail.com>
2023-02-09 00:36:18 +0100

    init

--- src_bmp384_arduino_spi/src_bmp384_arduino_spi.ino
+++ src_bmp384_arduino_spi/src_bmp384_arduino_spi.ino
@@ -16,30 +16,38 @@
 
 //Viki Board
 #if 1
-#define VOLT_SRC 8 /*Connect sensor voltage input vcc to this pin*/
-#define BMP_MOSI 9 
-#define BMP_SCK 10
+#define BMP_CS 8 
+#define BMP_SCK 9
+#define BMP_MOSI 10 
 #define BMP_MISO 11 
-#define BMP_CS 12 
+
 #endif
 
 #define SEALEVELPRESSURE_HPA (1013.25) 
-bool bmp_init_flag = false;
 Adafruit_BMP3XX bmp; 
 
 void init_bmp384()
 {
-        digitalWrite(BMP_CS, LOW); 
+        static bool bmp_init_flag = false;
+
+        if(bmp_init_flag) return;
+
+        digitalWrite(BMP_CS, LOW);
+         
         if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) 
         {  
             Serial.println("Could not find a valid BMP3 sensor, check wiring!"); 
             return;
         } 
     
-    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X); 
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+     delay(100); 
     bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X); 
+     delay(100);
     bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3); 
+     delay(100);
     bmp.setOutputDataRate(BMP3_ODR_50_HZ); 
+     delay(100);
     Serial.println("Init Success");
     delay(100);
     bmp_init_flag = true;
@@ -61,10 +69,8 @@ void setup()
 
 void loop() { 
   
-  if(!bmp_init_flag){
-    init_bmp384();
-  }
-
+  init_bmp384();
+  
   if (! bmp.performReading()) { 
     Serial.println("Failed to perform reading :("); 
     return; 
@@ -81,5 +87,5 @@ void loop() {
   Serial.println(" m"); 
   Serial.println(); 
 
-  delay(500); 
+  delay(1000); 
 } 
